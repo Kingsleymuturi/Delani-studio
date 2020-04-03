d1b1eb1a72fa8e494da945943df3b08f9b8c8ece
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-04-03 09:10:43 +0300

    add what we do section along with code in js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".clickable1").click(function() {
+    $(".development-text").slideToggle();
+    $(".development-icon").slideToggle()
+  });
+    $(".clickable2").click(function() {
+    $(".product-text").slideToggle();
+    $(".product-icon").slideToggle()
+  });
+    $(".clickable").click(function() {
+    $(".design-text").slideToggle();
+    $(".design-icon").slideToggle()
+  });
+})
