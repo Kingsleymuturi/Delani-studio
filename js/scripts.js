bc7f61e470865aabfe39cbc1d35f77f1a0cd9f40
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-04-04 22:11:12 +0300

    add contact us function in js and details

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,29 @@ $(document).ready(function() {
       speed: 200
     })
 })
+
+document.getElementById("submit").addEventListener("click",function(event){
+  event.preventDefault()
+})
+function validateForm(){
+  var username = $("#NAME").val();
+  var useremail = $("#EMAIL").val();
+  var usermessage = $("#MESSAGE").val();
+
+  if (username === ""){
+  document.getElementById("Invalid").innerText = "Please enter your name!"
+  document.getElementById("Invalid").style.color ="blue"
+} else if(useremail === ""){
+  document.getElementById("Invalid1").innerText = "Please enter your email!"
+  document.getElementById("Invalid1").style.color ="blue"
+} else if(usermessage === ""){
+  document.getElementById("Invalid2").innerText = "Please type your message!"
+  document.getElementById("Invalid2").style.color ="blue"
+} else{
+  document.getElementById("contact").innerHTML = "Thank you for your feedback!"
+  document.getElementById("contact").style.fontSize = "50px"
+  document.getElementById("contact").style.textAlign = "center"
+  document.getElementById("contact").style.color ="White"
+  alert("Thank you for reaching to us " + username +  ". Your message has been received by us and is being reviewed")
+};
+};
